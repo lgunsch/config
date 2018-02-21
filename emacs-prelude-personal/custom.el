ee2cb15d55f4c104383a683b9704ebe653ace6de
Lewis Gunsch <lewis.gunsch@thoughtexchange.com>
2018-02-21 11:00:15 -0700

    Add in prelude-emacs personal config

---
+++ emacs-prelude-personal/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (ansible indent-tools smooth-scrolling highlight-indentation helm-swoop neotree use-package yaml-mode web-mode geiser zop-to-char zenburn-theme which-key volatile-highlights undo-tree smartrep smartparens smart-mode-line rainbow-mode rainbow-delimiters racer ov operate-on-number move-text magit json-mode js2-mode imenu-anywhere helm-projectile helm-descbinds helm-ag guru-mode grizzl gotest god-mode go-projectile gitignore-mode gitconfig-mode git-timemachine gist flycheck-rust expand-region elisp-slime-nav editorconfig easy-kill discover-my-major diminish diff-hl crux counsel company-go company-anaconda cargo browse-kill-ring beacon anzu ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
